ae5eab5a05b90dfd82f6aaac895ed1f7a199f071
WaheedHafez <48513225+WaheedHafez@users.noreply.github.com>
2022-10-03 17:40:32 +0200

    implement KeyExpansion

---
+++ src/keychain/KeyExpansion.java
@@ -0,0 +1,41 @@
+package keychain;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+class KeyExpansion {
+
+    static SecretKey[] expand(SecretKey sk, int n) {
+        // inspired from https://github.com/square/keywhiz/blob/master/hkdf/src/main/java/keywhiz/hkdf/Hkdf.java#L102
+
+        SecretKey[] secretKeys = new SecretKey[n];
+
+        Mac prf = getMac();
+        try {
+            prf.init(sk);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < n; i++) {
+            prf.reset();
+            byte[] derivedKey = prf.doFinal(new byte[]{(byte) i});
+            secretKeys[i] = new SecretKeySpec(derivedKey, "");
+        }
+
+        return secretKeys;
+    }
+
+    private static Mac getMac() {
+        var algorithm = "HmacSHA256";
+
+        try {
+            return Mac.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/keychain/KeyExpansionTest.java
@@ -0,0 +1,23 @@
+package keychain;
+
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class KeyExpansionTest {
+
+    @Test
+    void expand() {
+        SecretKey secretKey = new SecretKeyFromPassword("password1234!".toCharArray()).dSecretKey;
+        int n = 5;
+        SecretKey[] secretKeys = KeyExpansion.expand(secretKey, n);
+        assertEquals(n, secretKeys.length);
+
+        for (SecretKey k : secretKeys) {
+            assertNotNull(k);
+        }
+    }
+}
